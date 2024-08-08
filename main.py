d01732ceda36a3a5f6b49f341986bf7ed532a123
TWilhem <tom.wilhem@etu.umontpellier.fr>
2024-08-08 22:48:32 +0200

    ajout prise en charge erreur

--- main.py
+++ main.py
@@ -20,21 +20,24 @@ Liste_Car = []
 with open(f"./SAE-Car.txt", "w", encoding='utf8') as VALUES_Car:
     
     for data in response:
-        car={
-            "name": data['name']["value"],
-            "type": data['type'],
-            "allowedVehicleType": data["allowedVehicleType"]["value"],
-            "availableSpotNumber": data['availableSpotNumber']['value'],
-            "totalSpotNumber": data['totalSpotNumber']['value'],  
-            "category": data['category']["value"],
-            "longitude": [data['location']['value']['coordinates'][1]],
-            "latitude": [data['location']['value']['coordinates'][0]],
-            "requiredPermit": data['requiredPermit']['value'],
-            "status": data['status']["value"],
-            "Date": Date(),
-            "Hour": Hour(),
-        }
-        Liste_Car.append(car)
+        try:
+            car={
+                "name": data['name']["value"],
+                "type": data['type'],
+                "allowedVehicleType": data["allowedVehicleType"]["value"],
+                "availableSpotNumber": data['availableSpotNumber']['value'],
+                "totalSpotNumber": data['totalSpotNumber']['value'],  
+                "category": data['category']["value"],
+                "longitude": [data['location']['value']['coordinates'][1]],
+                "latitude": [data['location']['value']['coordinates'][0]],
+                "requiredPermit": data['requiredPermit']['value'],
+                "status": data['status']["value"],
+                "Date": Date(),
+                "Hour": Hour(),
+            }
+            Liste_Car.append(car)
+        except KeyError:
+            continue
     NewdataCar=Liste_Car 
 
     VALUES_Car.writelines(json.dumps(NewdataCar, ensure_ascii=False, indent=4))
@@ -48,19 +51,22 @@ Liste_Velo = []
 with open(f"./SAE-Bike.txt", "w", encoding='utf8') as VALUES_Bike:
     
     for data in response_2:
-        bike={
-            "id": data['id'],
-            "type": data['type'],
-            "address": data['address']['value']['streetAddress'],
-            "availableBikeNumber": data["availableBikeNumber"]['value'],
-            "totalSlotNumber": data['totalSlotNumber']['value'],
-            "longitude": [data['location']['value']['coordinates'][1]],
-            "latitude": [data['location']['value']['coordinates'][0]],
-            "status": data['status']['value'],
-            "Date": Date(),
-            "Hour": Hour(),
-        }
-        Liste_Velo.append(bike)
+        try:
+            bike={
+                "id": data['id'],
+                "type": data['type'],
+                "address": data['address']['value']['streetAddress'],
+                "availableBikeNumber": data["availableBikeNumber"]['value'],
+                "totalSlotNumber": data['totalSlotNumber']['value'],
+                "longitude": [data['location']['value']['coordinates'][1]],
+                "latitude": [data['location']['value']['coordinates'][0]],
+                "status": data['status']['value'],
+                "Date": Date(),
+                "Hour": Hour(),
+            }
+            Liste_Velo.append(bike)
+        except KeyError:
+            continue
     NewdataBike=Liste_Velo
 
     VALUES_Bike.writelines(json.dumps(NewdataBike, ensure_ascii=False, indent=4))
