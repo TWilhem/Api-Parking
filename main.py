3f23722f7115a0bed8099760d481c92bdc5f524b
TWilhem <tom.wilhem@etu.umontpellier.fr>
2024-08-11 03:52:33 +0200

    Ajout de prise en charge des liens cassées

--- main.py
+++ main.py
@@ -13,60 +13,67 @@ def Hour():
     now = datetime.datetime.now(paris_tz)
     return now.strftime("%H:%M:%S")
 
+try:
 
-response=requests.get("https://portail-api-data.montpellier3m.fr/offstreetparking?limit=1000").json()
+    response=requests.get("https://portail-api-data.montpellier3m.fr/offstreetparking?limit=1000").json()
 
-Liste_Car = []
-with open(f"./SAE-Car.txt", "w", encoding='utf8') as VALUES_Car:
-    
-    for data in response:
-        try:
-            car={
-                "name": data['name']["value"],
-                "type": data['type'],
-                "allowedVehicleType": data["allowedVehicleType"]["value"],
-                "availableSpotNumber": data['availableSpotNumber']['value'],
-                "totalSpotNumber": data['totalSpotNumber']['value'],  
-                "category": data['category']["value"],
-                "longitude": [data['location']['value']['coordinates'][1]],
-                "latitude": [data['location']['value']['coordinates'][0]],
-                "requiredPermit": data['requiredPermit']['value'],
-                "status": data['status']["value"],
-                "Date": Date(),
-                "Hour": Hour(),
-            }
-            Liste_Car.append(car)
-        except KeyError:
-            continue
-    NewdataCar=Liste_Car 
+    Liste_Car = []
+    with open(f"./SAE-Car.txt", "w", encoding='utf8') as VALUES_Car:
+        
+        for data in response:
+            try:
+                car={
+                    "name": data['name']["value"],
+                    "type": data['type'],
+                    "allowedVehicleType": data["allowedVehicleType"]["value"],
+                    "availableSpotNumber": data['availableSpotNumber']['value'],
+                    "totalSpotNumber": data['totalSpotNumber']['value'],  
+                    "category": data['category']["value"],
+                    "longitude": [data['location']['value']['coordinates'][1]],
+                    "latitude": [data['location']['value']['coordinates'][0]],
+                    "requiredPermit": data['requiredPermit']['value'],
+                    "status": data['status']["value"],
+                    "Date": Date(),
+                    "Hour": Hour(),
+                }
+                Liste_Car.append(car)
+            except KeyError:
+                continue
+        NewdataCar=Liste_Car 
 
-    VALUES_Car.writelines(json.dumps(NewdataCar, ensure_ascii=False, indent=4))
+        VALUES_Car.writelines(json.dumps(NewdataCar, ensure_ascii=False, indent=4))
+
+except requests.exceptions.RequestException:
+    print("Impossible d'accéder aux données de stationnement pour voitures. Tentative d'accès aux données de vélos...")
 
 
+try:
 
+    response_2 = requests.get('https://portail-api-data.montpellier3m.fr/bikestation?limit=1000').json()
 
-response_2 = requests.get('https://portail-api-data.montpellier3m.fr/bikestation?limit=1000').json()
+    Liste_Velo = []
+    with open(f"./SAE-Bike.txt", "w", encoding='utf8') as VALUES_Bike:
+        
+        for data in response_2:
+            try:
+                bike={
+                    "id": data['id'],
+                    "type": data['type'],
+                    "address": data['address']['value']['streetAddress'],
+                    "availableBikeNumber": data["availableBikeNumber"]['value'],
+                    "totalSlotNumber": data['totalSlotNumber']['value'],
+                    "longitude": [data['location']['value']['coordinates'][1]],
+                    "latitude": [data['location']['value']['coordinates'][0]],
+                    "status": data['status']['value'],
+                    "Date": Date(),
+                    "Hour": Hour(),
+                }
+                Liste_Velo.append(bike)
+            except KeyError:
+                continue
+        NewdataBike=Liste_Velo
 
-Liste_Velo = []
-with open(f"./SAE-Bike.txt", "w", encoding='utf8') as VALUES_Bike:
-    
-    for data in response_2:
-        try:
-            bike={
-                "id": data['id'],
-                "type": data['type'],
-                "address": data['address']['value']['streetAddress'],
-                "availableBikeNumber": data["availableBikeNumber"]['value'],
-                "totalSlotNumber": data['totalSlotNumber']['value'],
-                "longitude": [data['location']['value']['coordinates'][1]],
-                "latitude": [data['location']['value']['coordinates'][0]],
-                "status": data['status']['value'],
-                "Date": Date(),
-                "Hour": Hour(),
-            }
-            Liste_Velo.append(bike)
-        except KeyError:
-            continue
-    NewdataBike=Liste_Velo
+        VALUES_Bike.writelines(json.dumps(NewdataBike, ensure_ascii=False, indent=4))
 
-    VALUES_Bike.writelines(json.dumps(NewdataBike, ensure_ascii=False, indent=4))
+except requests.exceptions.RequestException:
+    print("Impossible d'accéder aux données de stationnement pour vélos. Arrêt du script.")
