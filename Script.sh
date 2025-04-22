defa1f27657f34503b8ed1aa10188f6c5d7eea2f
TWilhem <tom.wilhem@etu.umontpellier.fr>
2025-04-23 00:12:16 +0200

    changement Script bash dossier configuration

--- Script.sh
+++ Script.sh
@@ -181,7 +181,7 @@ unzip File.zip
 rm File.zip
 
 mv Api-Parking-main "$LOCAL_DIR"
-mv $LOCAL_DIR/Api-Parking-Deploiement/* $LOCAL_DIR/Api-Parking/
+mv ./Api-Parking-Deploiement/* ./Api-Parking/
 rm -rf ./Api-Parking-Deploiement
 rm -rf ./Api-Parking/docs/Analyse 
 rm -rf ./Api-Parking/TP
